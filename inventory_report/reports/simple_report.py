de594d715d96d757f8524669ce766d478ecc0436
Brayan Santos <brayan.santosrosa@gmail.com>
2023-03-03 18:57:03 -0300

    Requisito 02 concluido

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,46 @@
+from datetime import datetime
+
+
 class SimpleReport:
-    pass
+    @classmethod
+    def generate(cls, lista):
+        return f"""{cls.mais_antiga(lista)}
+{cls.data_mais_atual(lista)}
+{cls.empresa_mais_produtos(lista)}"""
+
+    @classmethod
+    def mais_antiga(cls, lista):
+        older = datetime.today().date()
+        for obj in lista:
+            fabrication = obj["data_de_fabricacao"]
+            if datetime.strptime(fabrication, "%Y-%m-%d").date() <= older:
+                older = datetime.strptime(fabrication, "%Y-%m-%d").date()
+        return f"Data de fabricação mais antiga: {older}"
+
+    @classmethod
+    def data_mais_atual(cls, lista):
+        list_dates = []
+        today = datetime.today().date()
+        for obj in lista:
+            validate = obj["data_de_validade"]
+            if datetime.strptime(validate, "%Y-%m-%d").date() > today:
+                list_dates.append(validate)
+        result = min(list_dates)
+        return f"Data de validade mais próxima: {result}"
+
+    @classmethod
+    def empresa_mais_produtos(cls, lista):
+        emprises = {}
+        for obj in lista:
+            name = obj["nome_da_empresa"]
+            if name not in emprises:
+                emprises[name] = 1
+            else:
+                emprises[name] += 1
+
+        result = sorted(
+            emprises.items(), 
+            key=lambda item: item[1], reverse=True)[0][0]
+        return f'Empresa com mais produtos: {result}'
+
+
